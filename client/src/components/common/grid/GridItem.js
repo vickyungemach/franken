076f330ad4f79ff850d75b525aa0f5d696cff682
vicky <vicky.ungemach95@gmail.com>
2022-04-14 15:16:10 -0700

    MIN-120 determine image height and with

--- client/src/components/common/grid/GridItem.js
+++ client/src/components/common/grid/GridItem.js
@@ -7,7 +7,7 @@ const GridItem = ({ openGridModal, image, i }) => {
             key={i} 
             onClick={() => openGridModal(image, i)} 
             className="grid__body-item" 
-            style={{ backgroundImage: `url(${image})`}}
+            style={{ backgroundImage: `url(${image.url})`}}
         >
         
         </div>
--- client/src/components/common/grid/GridModal.js
+++ client/src/components/common/grid/GridModal.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react';
 
-const GridModal = ({ images, currentImageIndex, nextPhoto, closeGridModal }) => {
+const GridModal = ({ images, currentImageIndex, closeGridModal }) => {
+
+    const [controlls, setControlls] = useState(true);
 
     useEffect(() => {
-        const unsubscribe = document.addEventListener('keydown', function(e) {
-            if(e.key === 'Escape') {
+        const unsubscribe = document.addEventListener('keydown', function (e) {
+            if (e.key === 'Escape') {
                 closeGridModal();
-            }
+            } 
         })
 
         return unsubscribe;
@@ -34,6 +36,15 @@ const GridModal = ({ images, currentImageIndex, nextPhoto, closeGridModal }) => 
         }
     }
 
+    // Get image height, width style
+    const getImageStyle = (orientation) => {
+        if(window.innerWidth < 600) {
+            return { width: '100%'}
+        } 
+
+        return orientation === 'landscape' ? { width: '100%'} :  { height: '100%'}
+    }
+
     // Previous Button
     const previous = () => {
         if (currentSlide <= 0) return;
@@ -48,34 +59,48 @@ const GridModal = ({ images, currentImageIndex, nextPhoto, closeGridModal }) => 
         currentSlide === images.length - 1 ? setCurrentSlide(1) : setCurrentSlide(currentSlide + 1);
     }
 
+    // Toggle Controls
+    const toggleControls = (e) => { 
+        if(!e.target.classList.contains('fas')) {
+            setControlls(!controlls)
+        }
+    }
+
     return (
-        <div className='grid__modal'>
-            <div className="carousel">
+        <div className='grid__modal' onClick={toggleControls}>
+            <div className="grid__modal-carousel">
 
                 {/* Slides */}
                 {images.map((image, i) => (
                     <div
                         key={i}
-                        className={"carousel-slide"}
+                        className="grid__modal-slide"
                         style={{ transform: `translateX(-${currentSlide * 100}%)`, transition: transition }}
                         onTransitionEnd={loopCarousel}
                         id={i}
                     >
                         {/* Slide Image */}
-                        <img src={image} alt="" />
+                        <img src={image.url} alt="" style={getImageStyle(image.orientation)} />
                     </div>
                 ))}
             </div>
 
             {/* Previous button */}
-            <button className="carousel-btn left" onClick={previous}>
+            <button className={`grid__modal-btn grid__modal-btn--left ${ controlls ? '' : 'hidden'}`} onClick={previous}>
                 <i className="fas fa-chevron-left"></i>
             </button>
 
             {/* Next button */}
-            <button className="carousel-btn right" onClick={next}>
+            <button className={`grid__modal-btn grid__modal-btn--right ${ controlls ? '' : 'hidden'}`} onClick={next}>
                 <i className="fas fa-chevron-right"></i>
             </button>
+
+            {/* Close button */}
+            <button className={`grid__modal-btn grid__modal-btn--close ${ controlls ? '' : 'hidden'}`} onClick={closeGridModal}>
+                <i className="fas fa-times"></i>
+            </button>
+
+
         </div>
     )
 }
