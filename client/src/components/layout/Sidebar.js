bc9cd814650e16ab34811beb8ad150cef38ed501
vicky <vicky.ungemach95@gmail.com>
2022-05-11 06:52:22 -0700

    MIN-154 make sidebar functional

--- client/src/components/layout/Sidebar.js
+++ client/src/components/layout/Sidebar.js
@@ -1,15 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 import { Home, HomeOutline, Grid, GridOutline, Pricetag, Add, PricetagOutline } from 'react-ionicons'
 
 const Sidebar = () => {
 
+    const path = window.location.pathname;
+
     const [active, setActive] = useState(0);
 
-    const onClick = (page) => {
-        setActive(page);
-    }
+
+    useEffect(() => {
+        if (path === '/' ) {
+            setActive(0);
+        } else if(path.includes('groups')) {
+            setActive(1)
+        }
+    })
 
     const getClasses = (index) => {
         return index === active ? 'sidebar__link sidebar__link--active' : 'sidebar__link';
@@ -19,18 +26,18 @@ const Sidebar = () => {
     return (
         <ul className='sidebar'>
             <Link to="/">
-                <li className={getClasses(0)} onClick={() => onClick(0)}>
+                <li className={getClasses(0)} >
                     {active === 0 ? <Home height="17px" /> : <HomeOutline height="17px" />}
                     <p>Photos</p>
                 </li>
             </Link>
             <Link to="/groups">
-                <li className={getClasses(1)} onClick={() => onClick(1)}>
+                <li className={getClasses(1)} >
                     {active === 1 ? <Grid height="17px" /> : <GridOutline height="17px" />}
                     <p>Groups</p>
                 </li>
             </Link>
-            <li className={getClasses(2)} onClick={() => onClick(2)}>
+            <li className={getClasses(2)} >
                 {active === 2 ? <Pricetag height="17px" /> : <PricetagOutline height="17px" />}
                 <p>Tags</p>
             </li>
