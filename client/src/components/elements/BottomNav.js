4384dd68f725f6362b6cc601b1e55828f2de943e
vicky <vicky.ungemach95@gmail.com>
2022-05-25 12:21:47 -0700

    MIN-171 fix bottom nav

--- client/src/components/elements/BottomNav.js
+++ client/src/components/elements/BottomNav.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
-import { Home, Grid, Pricetag, Add } from 'react-ionicons'
+import React, { useState, useEffect } from 'react';
+import { Home, Grid, Pricetag, Add, HomeOutline, GridOutline, PricetagOutline } from 'react-ionicons';
+import { Link } from 'react-router-dom';
 
 /* Props
 ============================================================= */
@@ -8,17 +9,31 @@ import { Home, Grid, Pricetag, Add } from 'react-ionicons'
 const BottomNav = () => {
 
     const navLinks = [
-        { icon: <Home height="1.8rem" />, text: 'Photos' },
-        { icon: <Grid height="1.8rem" />, text: 'Groups' },
-        { icon: <Pricetag height="1.8rem" />, text: 'Tag' },
-        { icon: <Add height="1.8rem" />, text: 'Add' }
+        { icon: <HomeOutline height="1.8rem" />, iconActive: <Home height="1.8rem" />, text: 'Photos', link: '/'},
+        { icon: <GridOutline height="1.8rem" />, iconActive: <Grid height="1.8rem" />, text: 'Groups', link: '/groups'},
+        { icon: <PricetagOutline height="1.8rem" />, iconActive: <Pricetag height="1.8rem" />, text: 'Tags', link: '/' },
+        { icon: <Add height="1.8rem" />, iconActive: <Add height="1.8rem" />, text: 'Upload' }
     ]
+
+    const url = window.location.origin;
+    const path = window.location.pathname;
+
+    useEffect(() => {
+        if (path === '/') {
+            setActive(0);
+        } else if (path.includes('groups')) {
+            setActive(1)
+        }
+    })
 
     const [active, setActive] = useState(0);
 
     const activateTab = (e, i) => {
         e.preventDefault();
-        setActive(i);
+        if (i !== 3) {
+            setActive(i);
+            window.location.href = url + navLinks[i].link
+        }
     }
 
     return (
@@ -26,14 +41,14 @@ const BottomNav = () => {
 
             {
                 navLinks.map((link, i) => (
-                    <button 
-                        key={i} 
-                        href="" 
-                        className={`bottom-nav-link ${active === i ? 'bottom-nav-link--active' : ''}`} 
+                    <button
+                        key={i}
+                        href=""
+                        className={`bottom-nav-link ${active === i && 'bottom-nav-link--active'}`}
                         onClick={(e) => activateTab(e, i)}
                     >
-                        { link.icon }
-                        <span className="bottom-nav-text">{ link.text }</span>
+                        {active === i ? link.iconActive : link.icon}
+                        <span className='bottom-nav-text'>{link.text}</span>
                     </button>
                 ))
             }
